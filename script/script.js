ce1fff626c8cb06e330a1eb8c0be368b5bbf16c7
haoqianpro <18109692046@163.com>
2022-09-28 16:51:46 +0800

    加一段话

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
-const BEGAIN_WORK_DATE_STRING = '2022-05-11'
-const begainWorkDate = dayjs(BEGAIN_WORK_DATE_STRING)
-const today = dayjs()
+const BEGAIN_WORK_DATE_STRING = '2022-05-11';
+const begainWorkDate = dayjs(BEGAIN_WORK_DATE_STRING);
+const today = dayjs();
 
-const workCount = today.diff(begainWorkDate, 'day') + 1
+const workCount = today.diff(begainWorkDate, 'day') + 1;
 
-const span_today = document.getElementById('today')
-span_today.innerText = today.format('YYYY年MM月DD日')
+const span_today = document.getElementById('today');
+span_today.innerText = today.format('YYYY年MM月DD日');
 
-const span_workCount = document.getElementById('workCount')
-span_workCount.innerText = workCount
+const span_workCount = document.getElementById('workCount');
+span_workCount.innerText = workCount;
